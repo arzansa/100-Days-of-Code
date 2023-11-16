9d68f67c24ec9918b8b1b025e93616c2b108c6e9
arzansa <arzansa@gmail.com>
2023-11-15 21:36:08 -0600

    Challenge 2 completed

--- day-7/main.py
+++ day-7/main.py
@@ -1,19 +1,39 @@
 import random
 
-#Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
 random_word = random.randint(0, 2)
 word = word_list[random_word]
 print(word)
+spaces = []
+guessed = False
+game_end = False
+times_wrong = 0
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Guess a letter:').lower()
+for space in word:
+    spaces += '_'
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if (guess in word):
-    print('Right!')
-else:
-    print('Wrong!')
+while(not game_end):
+
+    print(spaces)
+    guess = input('Guess a letter: ').lower()
+
+    for i in range(len(word)):
+        if (guess == word[i]):
+            guessed = True
+            spaces[i] = word[i]
+
+    if (guessed is False):
+        print('Wrong!')
+        times_wrong += 1
+    guessed = False
+
+    if times_wrong is 6:
+        print('You lose!')
+        game_end = True
+
+    if '_' not in spaces:
+        print('You Win!')
+        game_end = True
